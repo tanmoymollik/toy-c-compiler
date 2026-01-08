3723b7060404cf2adc55a5d88db1ec6696fea7fa
Tanmoy Mollik <triploblastic@google.com>
2026-01-08 11:14:56 +0000

    Make disjointSet more efficient

--- lib/x64/disjointSet.ml
+++ lib/x64/disjointSet.ml
@@ -3,7 +3,10 @@ let union reg_map x y = Hashtbl.replace reg_map x y
 
 let rec find reg_map r =
   match Hashtbl.find_opt reg_map r with
-  | Some res -> find reg_map res
+  | Some res ->
+    let p = find reg_map res in
+    Hashtbl.replace reg_map r p;
+    p
   | None -> r
 ;;
 
